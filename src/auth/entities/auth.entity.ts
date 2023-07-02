864e44539176716546c416628a06fc4378c2acb9
dfrison12 <dariohfrison@gmail.com>
2023-07-02 19:03:54 -0300

    add config typeOrm for User

--- src/auth/entities/auth.entity.ts
+++ src/auth/entities/auth.entity.ts
@@ -1,20 +1,28 @@
 //nota: la funcion de la entidad es relacionar de alguna manera las tablas de nuestra base de datos y nuestra aplicacion de nest.
 // Por lo tanto esta entidad corresponde a una tabla de la base de datos.
 
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('users')
 export class User {
   @PrimaryGeneratedColumn('uuid')
+  @Column('text', {
+    unique: true,
+  })
   id: string;
 
+  @Column('text')
   email: string;
 
+  @Column('text')
   password: string;
 
+  @Column('text')
   fullName: string;
 
+  @Column('bool', { unique: true })
   isActive: boolean;
 
+  @Column('text', { default: ['user'] })
   roles: string[];
 }
